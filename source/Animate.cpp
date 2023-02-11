541dedfa2bae1cf84e40f0e78a9599dd54a0e0f2
Brawl Test <119479716+Brawltest@users.noreply.github.com>
2023-02-11 15:11:20 +0100

    modified:   source/Animate.cpp

--- source/Animate.cpp
+++ source/Animate.cpp
@@ -1,13 +1,18 @@
 #include <citro2d.h>
 
 typedef Vector2 {int x, y;};
+typedef Rectangle {int left,top,right,bottom;};
 
-typedef AnimatedSprite{
+typedef Animation{
     C2D_Sprite frames[];
     C2D_SpriteSheet spriteSheet;
+    int totalFrames = C2D_SpriteSheetCount(C2D_SpriteSheet);
+    int speed;
 };
 
-class Object {
+class AnimObject {
     Vector2 pos;
-    AnimatedSprite AnimSpr;
+    Animation AnimSpr;
 }
+
+AnimObject mario = new AnimObject;
